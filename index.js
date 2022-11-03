6f2c593b45ef78ff9b173921a5644d2a1b3b69a5
Ather1230 <ather.biswas0057@gmail.com>
2022-11-04 01:31:27 +0600

    pagination added

--- index.js
+++ index.js
@@ -15,7 +15,23 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.SECURITY_KEY}@cluster0.ukuxru5.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run(){
+    try{
+        const productCollection = client.db('emajhon').collection('products');
 
+        app.get('/products', async(req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            const count = await productCollection.estimatedDocumentCount();
+            res.send({count, products});
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(err => console.error(err))
 
 app.get('/', (req, res) => {
     res.send('ema-jhon-server is running')
